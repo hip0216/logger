f7fbca9e974db2ff1aa4456248365bbe269a4035
alan.yang <alan.yang@104.com.tw>
2020-04-07 01:18:58 +0800

    append annotation in Logger.php

--- saveLoggerPlace/Logger.php
+++ saveLoggerPlace/Logger.php
@@ -4,13 +4,19 @@ namespace Php\Exam;
 
 use Psr\Log\LoggerInterface;
 
+/**
+ * this class give to run.php use
+ */
 class Logger implements LoggerInterface
 {
-    protected $connection=null;
-    function __construct()
+    protected $connection = null;
+    /**
+    *this method when class load will create database syslog.sqlite3 and create table logs
+    */
+    public function __construct()
     {
-        $this->connection=$connection=null;
-        $createLogsTableInstruction="CREATE TABLE IF NOT EXISTS logs(
+        $this->connection = $connection = null;
+        $createLogsTableInstruction = "CREATE TABLE IF NOT EXISTS logs(
          id INTEGER PRIMARY KEY AUTOINCREMENT,
          level VARCHAR(10) NOT NULL,
          message TEXT NOT NULL
@@ -19,14 +25,20 @@ class Logger implements LoggerInterface
         $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $this->connection->exec($createLogsTableInstruction);
     }
-    private function insertValuesToLogsTable($useFuncName,$context)
+    /**
+    *this method give to run.php insert attribute to logs table
+    */
+    private function insertValuesToLogsTable($useFuncName, $context)
     {
-        $insertToLogsTable="INSERT INTO logs (level,message) VALUES (:level,:message)";
-        $insertSQLInstruction=$this->connection->prepare($insertToLogsTable);
+        $insertToLogsTable = "INSERT INTO logs (level,message) VALUES (:level,:message)";
+        $insertSQLInstruction = $this->connection->prepare($insertToLogsTable);
         $insertSQLInstruction->bindValue(':level', $useFuncName, \PDO::PARAM_STR);
         $insertSQLInstruction->bindValue(':message', $context, \PDO::PARAM_STR);
         $insertSQLInstruction->execute();
     }
+    /**
+    *these method from LoggerInterface implements to  run.php insert attribute
+    */
     public function debug($message, array $context = array())
     {
         $this->insertValuesToLogsTable('debug', $message);
@@ -47,20 +59,19 @@ class Logger implements LoggerInterface
     {
         $this->insertValuesToLogsTable('info', $message);
     }
+    /**
+    *these method from LoggerInterface implements are not give to run.php
+    */
     public function warning($message, array $context = array())
     {
-      
     }
     public function alert($message, array $context = array())
     {
-      
     }
     public function emergency($message, array $context = array())
     {
-      
     }
-    public function log($level,$message, array $context = array())
+    public function log($level, $message, array $context = array())
     {
-      
     }
 }
